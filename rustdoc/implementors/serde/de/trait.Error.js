c9df9398a3a8774719eb93108b70da50c3440608
sunshowers <rain@sunshowers.io>
2022-08-12 21:27:18 +0000

    Deploying to gh-pages from @ nextest-rs/nextest@f4e5d385bf0703e7e4e7445f62bda6cdca612c0e 🚀

--- rustdoc/implementors/serde/de/trait.Error.js
+++ rustdoc/implementors/serde/de/trait.Error.js
@@ -3,4 +3,5 @@ implementors["config"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait
 implementors["serde"] = [];
 implementors["serde_json"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>","synthetic":false,"types":["serde_json::error::Error"]}];
 implementors["toml"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"toml/de/struct.Error.html\" title=\"struct toml::de::Error\">Error</a>","synthetic":false,"types":["toml::de::Error"]}];
+implementors["toml_edit"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"toml_edit/de/struct.Error.html\" title=\"struct toml_edit::de::Error\">Error</a>","synthetic":false,"types":["toml_edit::de::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
